ecfe1662f30d05f3b9ae6bf25514d58ea3498446
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2023-01-03 21:11:33 +0000

    Add files via upload

---
+++ build/pm_proj2/CMakeFiles/detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detector.dir/src/detector.cpp.o"
+  "/home/percmap/project_pm2/devel/lib/pm_proj2/detector.pdb"
+  "/home/percmap/project_pm2/devel/lib/pm_proj2/detector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
