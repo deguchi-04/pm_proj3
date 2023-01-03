737169758296f8a55acbfa166f9cfb91a5037fae
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2023-01-03 21:12:55 +0000

    Add files via upload

---
+++ build/pm_proj2/CMakeFiles/risk_assessor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/risk_assessor.dir/src/risk_assessor.cpp.o"
+  "/home/percmap/project_pm2/devel/lib/pm_proj2/risk_assessor.pdb"
+  "/home/percmap/project_pm2/devel/lib/pm_proj2/risk_assessor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/risk_assessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
