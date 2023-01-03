9e486f5e8532ff415c41a266672ac7dde4274655
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2023-01-03 21:07:10 +0000

    Add files via upload

---
+++ build/pm_proj2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/percmap/project_pm2/src/pm_proj2
+# Build directory: /home/percmap/project_pm2/build/pm_proj2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
